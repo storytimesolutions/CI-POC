0d133a1144a148ac0e72044bd63485987bcc43c8
Chris Story <storytimesolutions@gmail.com>
2016-04-08 14:05:25 -0400

    Added comment

--- app/components/about/aboutController.js
+++ app/components/about/aboutController.js
@@ -4,6 +4,7 @@
     angular.module('app')
         .controller('AboutController', function(){
             var vm = this;
+            //Hello
             vm.developers = [
                 {name: 'Homer', bio: 'The father figure of the group', imageUrl: 'assets/img/homer.jpg'},
                 {name: 'Bart', bio: 'Young buck who does most of the work', imageUrl: 'assets/img/bart.png'}
