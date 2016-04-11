0d367aba8321603ef5e51a10a47083033aeb2706
Chris Story <storytimesolutions@gmail.com>
2016-04-11 13:47:48 -0400

    Learning how to BASH

--- scripts/travis/before_deploy.sh
+++ scripts/travis/before_deploy.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-set -e -o pipefail
+set -ev
 
 echo '*********************'
 echo '*** Before Deploy ***'
 echo '*********************'
 
 #Set environment variables
-echo 'TRAVIS_BUILD :'TRAVIS_BUILD
+echo "TRAVIS_BUILD : {TRAVIS_BUILD}"
 echo '$TRAVIS_BUILD :'$TRAVIS_BUILD
 echo '$AZURE_WA_USERNAME: '$AZURE_WA_USERNAME
+
+SITE=""
+BRANCH=""
+
+if [ "${TRAVIS_BRANCH}" = "master"]; then
+    SITE=$AZURE_WA_SITE_PROD
+    BRANCH="master"
+elif [ "${TRAVIS_BRANCH}" = "dev"]; then
+    SITE=$AZURE_WA_SITE_DEV
+    BRANCH="dev"
+fi
+
+export $azure_username=$AZURE_WA_USERNAME
+export $azure_password=$AZURE_WA_PASSWORD
+export $azure_site=SITE
+export $azure_on_branch=BRANCH
