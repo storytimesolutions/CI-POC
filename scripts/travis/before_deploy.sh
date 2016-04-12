b18dfacddbcdb2fc104442646af3b8a9def56461
Chris Story <storytimesolutions@gmail.com>
2016-04-12 07:41:40 -0400

    another try

--- scripts/travis/before_deploy.sh
+++ scripts/travis/before_deploy.sh
@@ -12,20 +12,22 @@ echo "AZURE_WA_USERNAME is $AZURE_WA_USERNAME"
 SITE=""
 BRANCH=""
 
-if [ $TRAVIS_BRANCH = "master" ]; then
+if [[ "$TRAVIS_BRANCH" = "master" ]]; then
     SITE=$AZURE_WA_SITE_PROD
     BRANCH="master"
-elif [ $TRAVIS_BRANCH = "dev" ]; then
+elif [[ "$TRAVIS_BRANCH" = "dev" ]]; then
     SITE=$AZURE_WA_SITE_DEV
     BRANCH="dev"
 fi
+
+echo "Branch is "BRANCH
 
 export azure_username=$AZURE_WA_USERNAME
 export azure_password=$AZURE_WA_PASSWORD
 export azure_site=SITE
 export azure_on_branch=BRANCH
 
-echo 'azure_username: 'azure_username
-echo 'azure_password: 'azure_password
-echo 'azure_site: 'azure_site
-echo 'azure_on_branch: 'azure_on_branch
+echo 'azure_username: '$azure_username
+echo 'azure_password: '$azure_password
+echo 'azure_site: '$azure_site
+echo 'azure_on_branch: '$azure_on_branch
