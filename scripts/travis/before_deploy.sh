48a47e27cd279ebb22397279d0a74c7abf42a7a9
Chris Story <storytimesolutions@gmail.com>
2016-04-13 17:18:49 -0400

    Added git commands to bash

--- scripts/travis/before_deploy.sh
+++ scripts/travis/before_deploy.sh
@@ -8,4 +8,6 @@ echo '*********************'
 echo 'Removing node_modules'
 rm -f -r node_modules
 rm -f index.html
+git add --all
+git commit -m 'Changed made by Travis deployment'
 echo 'Removed node_modules'
