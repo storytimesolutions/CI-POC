3fb562c2129ebbd2abf02b9bc7dacb757c437c88
Chris Story <storytimesolutions@gmail.com>
2016-04-13 17:26:05 -0400

    Adding skip cleanup

--- scripts/travis/before_deploy.sh
+++ scripts/travis/before_deploy.sh
@@ -8,8 +8,8 @@ echo '*********************'
 echo 'Removing node_modules'
 rm -f -r node_modules
 rm -f index.html
-git config --global user.email "travis-ci@example.com"
-git config --global user.name "Travis CI"
-git add --all
-git commit -m 'Changed made by Travis deployment'
+#git config --global user.email "travis-ci@example.com"
+#git config --global user.name "Travis CI"
+#git add --all
+#git commit -m 'Changed made by Travis deployment'
 echo 'Removed node_modules'
