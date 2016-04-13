fb961ec106009e932c247fb9b098d0b13e18edb1
Chris Story <storytimesolutions@gmail.com>
2016-04-13 15:48:52 -0400

    Removed Harp and have browser sync working and ready to chain events

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,18 @@ var jshint      = require('gulp-jshint');
 var stylish     = require('jshint-stylish');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var harp        = require('harp');
+var browserSync = require('browser-sync').create();
 var Server      = require('karma').Server;
 
 
 /*Gulp Final Product Desires
-1) Run local server in dev or prod version
-    a) Automatic Browser Refresh on Change
-2) Run Jasmine tests with each change
-3) Concat all angular app files into a dev versions 
-    a) File by type (Controllers?  Services?  All?)
-4) Minify concat files for Production
+XXX - 1) Run local server in dev or prod version
+XXX    a) Automatic Browser Refresh on Change
+XXX - 2) Run Jasmine tests with each change
+XXX - 3) Concat all angular app files into a dev versions 
+XXX - 4) Minify concat files for Production
 5) Move dependencies from packages to local directory for dev environment
+6) Get Prod Version working locally 
 
 Future thoughts
 1) Change out local project dependencies with CDNs on Prod deployment
@@ -51,8 +49,8 @@ gulp.task('clean', function(){
         .pipe(clean()); 
 });
 
-gulp.task('scripts',['clean'], function(){
-    gulp.src(paths.scripts)
+gulp.task('package-scripts',['clean'], function(){
+    return gulp.src(paths.scripts)
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(concat('app.min.js'))
@@ -61,41 +59,40 @@ gulp.task('scripts',['clean'], function(){
 });
 
 gulp.task('copy-html',['clean'], function(){
-    gulp.src(bases.src + '**/*.html')
+    return gulp.src(bases.src + '**/*.html')
         .pipe(gulp.dest(bases.dist));
 });
 
 gulp.task('copy-libs', function(){
-     gulp.src(paths.libs)
+     return gulp.src(paths.libs)
         .pipe(gulp.dest(bases.libs));
 });
 
+gulp.task('prod-watch', ['dist'], browserSync.reload);
+gulp.task('dev-watch', ['test'], browserSync.reload);
 
+/**
+ * Serve the Harp Site from the dist directory
+ */
+gulp.task('serve-prod', function () {
+    browserSync.init({
+        server: __dirname + '/dist'
+    });
+    
+    gulp.watch(["src/**/*.html", "src/**/*.js"], ['prod-watch']);
+    
+});
 
 /**
  * Serve the Harp Site from the src directory
  */
-gulp.task('serve-dev', function () {
-  harp.server(__dirname + '/src' , {
-    port: 9000
-  }, function () {
-    browserSync({
-      proxy: "localhost:9000",
-      open: true
+gulp.task('serve-dev', ['copy-libs'], function () {
+    browserSync.init({
+        server: __dirname + '/src'
     });
-    /**
-     * Watch for scss changes, tell BrowserSync to refresh main.css
-     */
-    gulp.watch(["*.css", "*.sass", "*.scss", "*.less"], function () {
-      reload("main.css", {stream: true});
-    });
-    /**
-     * Watch for all other changes, reload the whole page (could have .ejs, .jade, .json, .md)
-     */
-    gulp.watch(["src/app/**/*.html", "src/app/**/*.js"], function () {
-      reload();
-    });
-  })
+    
+    gulp.watch(["src/**/*.html", "src/**/*.js"], ['dev-watch']);
+    
 });
 
 //Run Jasmine Tests one-time with Karma
@@ -109,15 +106,15 @@ gulp.task('test', function(done){
 //Be in TDD mode (Run tests on every change)
 gulp.task('tdd', function(done){
     new Server({
-        configFile: __dirname + '/karma-js.conf.js'
+        configFile: __dirname + '/karma-js.conf.js',
+        singleRun: false
     }, done).start();
 });
 
-//Create dist
-gulp.task('dist', ['copy-html', 'scripts']);
+//Create distribution package
+gulp.task('dist', ['copy-html', 'package-scripts']);
 
 /**
- * Default task, running `gulp` will fire up the Harp site,
- * launch BrowserSync & watch files.
+ * Default task, running `gulp` will launch BrowserSync & watch files.
  */
-gulp.task('default', ['copy-libs', 'serve-dev', 'tdd']);
+gulp.task('default', ['serve-dev']);
